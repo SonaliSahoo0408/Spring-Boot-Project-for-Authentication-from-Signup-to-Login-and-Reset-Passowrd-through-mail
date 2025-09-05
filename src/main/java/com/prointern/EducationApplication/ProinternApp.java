b2eedcb07ab124920e5367c7f074e9fb563dcbf0
nimblixtechnologies <nimblixtechnologies@gmail.com>
2025-09-05 16:31:15 +0530

    Prointern Application

---
+++ src/main/java/com/prointern/EducationApplication/ProinternApp.java
@@ -0,0 +1,10 @@
+package com.prointern.EducationApplication;
+
+/**
+ * Hello world!
+ */
+public class ProinternApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
